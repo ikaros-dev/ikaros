6f595ccd1e584eb574d9085bb493be03ce88058c
li-guohao <git@liguohao.cn>
2022-05-21 23:35:28 +0800

    初始化仓库

---
+++ src/main/java/cn/liguohao/ikaros/IkarosApplication.java
@@ -0,0 +1,16 @@
+package cn.liguohao.ikaros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author liguohao
+ */
+@SpringBootApplication
+public class IkarosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IkarosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cn/liguohao/ikaros/IkarosApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.liguohao.ikaros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IkarosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
