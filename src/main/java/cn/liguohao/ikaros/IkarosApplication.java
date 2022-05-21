2e3eece8511fb4fa0d9f46a2eb61e4fc36030bef
li-guohao <git@liguohao.cn>
2022-05-21 23:59:27 +0800

    调整代码格式适应checkstyle规则

--- src/main/java/cn/liguohao/ikaros/IkarosApplication.java
+++ src/main/java/cn/liguohao/ikaros/IkarosApplication.java
@@ -4,13 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
+ * 伊卡洛斯启动
+ *
  * @author liguohao
  */
 @SpringBootApplication
 public class IkarosApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(IkarosApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(IkarosApplication.class, args);
+    }
 
 }
--- src/test/java/cn/liguohao/ikaros/IkarosApplicationTests.java
+++ src/test/java/cn/liguohao/ikaros/IkarosApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class IkarosApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
