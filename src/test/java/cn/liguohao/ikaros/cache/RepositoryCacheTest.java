83f6b2b440ba5bb2a19ecae11461a08acb573c28
li-guohao <git@liguohao.cn>
2022-06-03 13:00:26 +0800

    移除SpringCache

---
+++ src/test/java/cn/liguohao/ikaros/cache/RepositoryCacheTest.java
@@ -0,0 +1,32 @@
+package cn.liguohao.ikaros.cache;
+
+import cn.liguohao.ikaros.service.UserService;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * @author li-guohao
+ * @date 2022/06/03
+ */
+@Disabled
+@SpringBootTest
+public class RepositoryCacheTest {
+    private static final Logger LOGGER = LoggerFactory.getLogger(RepositoryCacheTest.class);
+
+    @Autowired
+    UserService userService;
+
+    @Test
+    void findByUsername() {
+        LOGGER.info("exec {}#findByUsername", this.getClass().getName());
+        final String username = "admin";
+        userService.findByUsername(username);
+        userService.findByUsername(username);
+        userService.findByUsername(username);
+    }
+
+}
