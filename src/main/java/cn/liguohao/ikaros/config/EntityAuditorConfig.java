0cbfe2c3c598fe99b35de49bfae59185d3e257ff
li-guohao <git@liguohao.cn>
2022-06-11 21:34:26 +0800

    修复日期获取可能的空值问题

--- src/main/java/cn/liguohao/ikaros/config/EntityAuditorConfig.java
+++ src/main/java/cn/liguohao/ikaros/config/EntityAuditorConfig.java
@@ -6,7 +6,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 
 /**
