704e87ffed4625cf2a66c947b7059c114c8874ad
li-guohao <git@liguohao.cn>
2022-11-20 01:41:04 +0800

    add app time zone setting

--- src/main/java/run/ikaros/server/IkarosApplication.java
+++ src/main/java/run/ikaros/server/IkarosApplication.java
@@ -2,6 +2,9 @@ package run.ikaros.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import run.ikaros.server.utils.StringUtils;
+
+import java.util.TimeZone;
 
 /**
  * Ikaros Main Class.
@@ -15,6 +18,14 @@ public class IkarosApplication {
         // 自定义额外的配置文件位置
         // System.setProperty("spring.config.additional-location",
         // SystemVarKit.getCurrentAppDirPath());
+
+        String timeZone = System.getenv("IKAROS_TIME_ZONE");
+        if (StringUtils.isBlank(timeZone)) {
+            timeZone = "Asia/Shanghai";
+        }
+
+        // 时区设置
+        TimeZone.setDefault(TimeZone.getTimeZone(timeZone));
 
         // 运行应用
         SpringApplication.run(IkarosApplication.class, args);
