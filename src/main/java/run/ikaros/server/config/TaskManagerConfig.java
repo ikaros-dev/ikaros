55197a033384fe58a07cf01320952c996125a987
li-guohao <git@liguohao.cn>
2022-11-23 00:52:31 +0800

    fix task manager not work normally issue

--- src/main/java/run/ikaros/server/config/TaskManagerConfig.java
+++ src/main/java/run/ikaros/server/config/TaskManagerConfig.java
@@ -27,110 +27,77 @@ import java.util.concurrent.TimeUnit;
 public class TaskManagerConfig {
     private static final Logger LOGGER = LoggerFactory.getLogger(TaskManagerConfig.class);
 
-    private final ScheduledThreadPoolExecutor scheduledThreadPoolExecutor;
-
-    private final ScheduledTaskService scheduledTaskService;
     private final OptionService optionService;
     private final TaskService taskService;
 
-    private ScheduledFuture<?> pullAnimeTaskScheduledFuture = null;
-    private ScheduledFuture<?> searchDownProcessTaskScheduledFuture = null;
 
-    public TaskManagerConfig(ScheduledThreadPoolExecutor scheduledThreadPoolExecutor,
-                             ScheduledTaskService scheduledTaskService, OptionService optionService,
+    public TaskManagerConfig(OptionService optionService,
                              TaskService taskService) {
-        this.scheduledThreadPoolExecutor = scheduledThreadPoolExecutor;
-        this.scheduledTaskService = scheduledTaskService;
         this.optionService = optionService;
         this.taskService = taskService;
     }
 
-    //    @Scheduled(cron = "0 */30 * * * ?")
-    //    public void halfHourOnceTask() {
-    //        taskService.pullAnimeSubscribeAndSaveMetadataAndDownloadTorrents();
-    //        taskService.searchDownloadProcessAndCreateFileHardLinksAndRelateEpisode();
-    //    }
-    //
-    //    @Scheduled(cron = "0 */5 * * * ?")
-    //    public void fiveMinuteOnceTask() {
-    //        qbittorrentClient.tryToResumeAllMissingFilesErroredTorrents();
-    //    }
-
-
-    @Scheduled(cron = "0 0 1 * * ?")
-    public void scheduledTaskManage() {
-        List<ScheduledTaskEntity> scheduledTaskEntities = scheduledTaskService.listAll();
-        for (ScheduledTaskEntity taskEntity : scheduledTaskEntities) {
-            if (LocalDateTime.now().isAfter(taskEntity.getDeadTime())) {
-                scheduledTaskService.stop(taskEntity);
-                scheduledTaskService.updateStatus(taskEntity.getId(), true);
-                LOGGER.info("删除过期定时任务成功，ID:{}，名称：{}，Cron：{}，开始时间：{}，截止时间：{}",
-                    taskEntity.getId(), taskEntity.getName(), taskEntity.getCron(),
-                    taskEntity.getStartTime(), taskEntity.getDeadTime());
-            } else {
-                LOGGER.info(
-                    "尝试启动尚未启动的定时任务，ID:{}，名称：{}，Cron：{}，开始时间：{}，截止时间：{}",
-                    taskEntity.getId(), taskEntity.getName(), taskEntity.getCron(),
-                    taskEntity.getStartTime(), taskEntity.getDeadTime());
-                scheduledTaskService.start(taskEntity);
-            }
+    private boolean appIsInit() {
+        try {
+            Boolean status = optionService.findOptionValueByCategoryAndKey(OptionCategory.APP,
+                OptionApp.IS_INIT.name()).getStatus();
+            LOGGER.debug("current app init status={}", status);
+            return status;
+        } catch (RecordNotFoundException recordNotFoundException) {
+            LOGGER.debug("app not init, skip config cron task: updateAutoAnimeSubTaskStatus");
+            return false;
         }
     }
 
-    @Scheduled(cron = "0 */5 * * * ?")
-    public void updateAutoAnimeSubTaskStatus() {
-        LOGGER.debug("exec config cron task: updateAutoAnimeSubTaskStatus");
-        try {
-            Boolean isInit = optionService.findOptionValueByCategoryAndKey(OptionCategory.APP,
-                OptionApp.IS_INIT.name()).getStatus();
-            if (!isInit) {
-                LOGGER.debug("app not init, skip config cron task: updateAutoAnimeSubTaskStatus");
-                return;
-            }
-        } catch (RecordNotFoundException recordNotFoundException) {
-            LOGGER.debug("app not init, skip config cron task: updateAutoAnimeSubTaskStatus");
+    @Scheduled(cron = "0 */30 * * * ?")
+    public void halfHourOnceTask() {
+        if (!appIsInit()) {
+            LOGGER.debug("app not init, skip config cron task: halfHourOnceTask");
             return;
         }
-
 
         String value = optionService.findOptionValueByCategoryAndKey(OptionCategory.APP,
             OptionApp.ENABLE_AUTO_ANIME_SUB_TASK.name()).getValue();
         if (Boolean.TRUE.toString().equalsIgnoreCase(value)) {
-            if (pullAnimeTaskScheduledFuture == null) {
-                pullAnimeTaskScheduledFuture =
-                    scheduledThreadPoolExecutor.scheduleAtFixedRate(
-                        taskService::pullAnimeSubscribeAndSaveMetadataAndDownloadTorrents,
-                        0, 30, TimeUnit.MINUTES);
-                LOGGER.debug("submit app scheduled task:  "
-                    + "pullAnimeSubscribeAndSaveMetadataAndDownloadTorrents");
-            }
+            taskService.pullAnimeSubscribeAndSaveMetadataAndDownloadTorrents();
+        }
 
-            if (searchDownProcessTaskScheduledFuture == null) {
-                searchDownProcessTaskScheduledFuture =
-                    scheduledThreadPoolExecutor.scheduleAtFixedRate(
-                        taskService::searchDownloadProcessAndCreateFileHardLinksAndRelateEpisode,
-                        0, 5, TimeUnit.MINUTES);
-                LOGGER.debug("submit app scheduled task:  "
-                    + "searchDownloadProcessAndCreateFileHardLinksAndRelateEpisode");
-            }
-        } else {
-            if (pullAnimeTaskScheduledFuture != null) {
-                if (!pullAnimeTaskScheduledFuture.isCancelled()) {
-                    pullAnimeTaskScheduledFuture.cancel(false);
-                    LOGGER.debug("cancel app scheduled task:  "
-                        + "pullAnimeSubscribeAndSaveMetadataAndDownloadTorrents");
-                    pullAnimeTaskScheduledFuture = null;
-                }
-            }
-            if (searchDownProcessTaskScheduledFuture != null) {
-                if (!searchDownProcessTaskScheduledFuture.isCancelled()) {
-                    searchDownProcessTaskScheduledFuture.cancel(false);
-                    LOGGER.debug("cancel app scheduled task:  "
-                        + "searchDownProcessTaskScheduledFuture");
-                    searchDownProcessTaskScheduledFuture = null;
-                }
-            }
+    }
+
+    @Scheduled(cron = "0 */5 * * * ?")
+    public void fiveMinuteOnceTask() {
+        if (!appIsInit()) {
+            LOGGER.debug("app not init, skip config cron task: fiveMinuteOnceTask");
+            return;
+        }
+
+        String value = optionService.findOptionValueByCategoryAndKey(OptionCategory.APP,
+            OptionApp.ENABLE_AUTO_ANIME_SUB_TASK.name()).getValue();
+        if (Boolean.TRUE.toString().equalsIgnoreCase(value)) {
+            taskService.searchDownloadProcessAndCreateFileHardLinksAndRelateEpisode();
         }
     }
+
+
+    //    @Scheduled(cron = "0 0 1 * * ?")
+    //    public void scheduledTaskManage() {
+    //        List<ScheduledTaskEntity> scheduledTaskEntities = scheduledTaskService.listAll();
+    //        for (ScheduledTaskEntity taskEntity : scheduledTaskEntities) {
+    //            if (LocalDateTime.now().isAfter(taskEntity.getDeadTime())) {
+    //                scheduledTaskService.stop(taskEntity);
+    //                scheduledTaskService.updateStatus(taskEntity.getId(), true);
+    //                LOGGER.info("删除过期定时任务成功，ID:{}，名称：{}，Cron：{}，开始时间：{}，截止时间：{}",
+    //                    taskEntity.getId(), taskEntity.getName(), taskEntity.getCron(),
+    //                    taskEntity.getStartTime(), taskEntity.getDeadTime());
+    //            } else {
+    //                LOGGER.info(
+    //                    "尝试启动尚未启动的定时任务，ID:{}，名称：{}，Cron：{}，开始时间：{}，截止时间：{}",
+    //                    taskEntity.getId(), taskEntity.getName(), taskEntity.getCron(),
+    //                    taskEntity.getStartTime(), taskEntity.getDeadTime());
+    //                scheduledTaskService.start(taskEntity);
+    //            }
+    //        }
+    //    }
+
 
 }
