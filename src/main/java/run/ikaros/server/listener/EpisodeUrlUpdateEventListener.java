b1ed8155b67cbd1872d3f7233fc127ceb898c6f0
li-guohao <46225881+li-guohao@users.noreply.github.com>
2022-12-14 15:45:16 +0800

    Revert "remove cover url in mail content for 544 issue (#174)" (#175)
    This reverts commit dc0382e7cede091ff22dc391ba39543d50aaa179.

--- src/main/java/run/ikaros/server/listener/EpisodeUrlUpdateEventListener.java
+++ src/main/java/run/ikaros/server/listener/EpisodeUrlUpdateEventListener.java
@@ -90,6 +90,7 @@ public class EpisodeUrlUpdateEventListener
             vars.put("epSeq", episodeEntity.getSeq());
             vars.put("epIntroduction", episodeEntity.getOverview());
             vars.put("introduction", animeEntity.getOverview());
+            vars.put("coverImgUrl", animeEntity.getCoverUrl());
             context.setVariables(vars);
 
             notifyService.sendTemplateMail(targetAddress, subject,
