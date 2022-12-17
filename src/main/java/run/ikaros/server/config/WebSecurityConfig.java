087f8b692e8ffef3cd821f1273b007b22c689a31
li-guohao <git@liguohao.cn>
2022-12-17 09:49:20 +0800

    fix spring security config issues

--- src/main/java/run/ikaros/server/config/WebSecurityConfig.java
+++ src/main/java/run/ikaros/server/config/WebSecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -48,6 +49,12 @@ public class WebSecurityConfig {
     @Bean
     public PasswordEncoder injectPasswordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(
+        AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
     }
 
     @Bean
@@ -96,7 +103,8 @@ public class WebSecurityConfig {
             .sessionManagement()
             .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
             .and()
-            .apply(securityConfigurationAdapter(http.getSharedObject(AuthenticationManager.class)));
+            .apply(securityConfigurationAdapter(
+                authenticationManager(http.getSharedObject(AuthenticationConfiguration.class))));
         return http.build();
     }
 
