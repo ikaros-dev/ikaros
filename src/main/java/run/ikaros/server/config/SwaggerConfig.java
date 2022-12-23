2bc32074f4b9474067d1e887116749844c098c77
li-guohao <git@liguohao.cn>
2022-12-23 15:59:39 +0800

    update springdoc dependency that support springboot3.0

--- src/main/java/run/ikaros/server/config/SwaggerConfig.java
+++ src/main/java/run/ikaros/server/config/SwaggerConfig.java
@@ -7,7 +7,7 @@ import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
-import org.springdoc.core.GroupedOpenApi;
+import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import run.ikaros.server.constants.AppConst;
