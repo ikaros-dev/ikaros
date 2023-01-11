cfb9de4a215e0cc6c05f4795f4b864b7a20aaee1
li-guohao <git@liguohao.cn>
2023-01-11 09:29:50 +0800

    fix PagingWrapTest#equals case

--- src/test/java/run/ikaros/server/core/warp/PagingWrapTest.java
+++ src/test/java/run/ikaros/server/core/warp/PagingWrapTest.java
@@ -118,7 +118,6 @@ class PagingWrapTest {
     void equals() {
         List<String> strList = List.of("1", "2", "3", "4");
         PagingWrap<String> pagingWrap = new PagingWrap<String>(1, 4, 10, strList);
-        assertThat(pagingWrap.canEqual(new Object())).isFalse();
         assertThat(pagingWrap.equals(PagingWrap.emptyResult())).isFalse();
         assertThat(pagingWrap.equals(new PagingWrap<String>(1, 4, 10, strList))).isTrue();
         assertThat(pagingWrap.equals(new PagingWrap<String>(1, 5, 10, strList))).isFalse();
