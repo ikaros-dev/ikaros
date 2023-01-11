231479abf8a28b228d06490347dae6c94962a8d4
li-guohao <git@liguohao.cn>
2023-01-11 17:58:21 +0800

    add some annotation in IkarosExtensionFactory

--- src/main/java/run/ikaros/server/plugin/IkarosExtensionFactory.java
+++ src/main/java/run/ikaros/server/plugin/IkarosExtensionFactory.java
@@ -1,7 +1,17 @@
 package run.ikaros.server.plugin;
 
 import org.pf4j.ExtensionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.AutowireCapableBeanFactory;
 
+/**
+ * <p>Basic implementation for {@link ExtensionFactory}.</p>
+ * <p>Uses Springs {@link AutowireCapableBeanFactory} to instantiate a given extension class,
+ * so can support {@link Autowired}.</p>
+ *
+ *
+ * @author li-guohao
+ */
 public class IkarosExtensionFactory implements ExtensionFactory {
     @Override
     public <T> T create(Class<T> extensionClass) {
