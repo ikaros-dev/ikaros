45969ce85515b8dc76f777999083064afd1f8081
li-guohao <46225881+li-guohao@users.noreply.github.com>
2023-02-12 15:25:39 +0800

    Subject relation crud (#294)
    * Add some subject relation service methods in SubjectRelationService and finish method unit test cases
    
    * Add some subject relation endpoint and unit test cases
    
    * Optimize SubjectRelationEndpoint that try test with swagger-ui web ui
    
    * Update CHANGELOG.MD for PR #294

---
+++ src/main/java/run/ikaros/server/core/subject/SubjectRelation.java
@@ -0,0 +1,36 @@
+package run.ikaros.server.core.subject;
+
+import static io.swagger.v3.oas.annotations.media.Schema.RequiredMode.REQUIRED;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
+import java.util.Set;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+import run.ikaros.server.store.enums.SubjectRelationType;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Accessors(chain = true)
+public class SubjectRelation {
+    @Schema(requiredMode = REQUIRED)
+    private Long subject;
+
+    /**
+     * Subject relation type.
+     *
+     * @see SubjectRelationType#getCode()
+     */
+    @Schema(requiredMode = REQUIRED)
+    @JsonProperty("relation_type")
+    private SubjectRelationType relationType;
+
+    @JsonProperty("relation_subjects")
+    @Schema(requiredMode = REQUIRED)
+    private Set<Long> relationSubjects;
+}
---
+++ src/main/java/run/ikaros/server/core/subject/SubjectRelationServiceImpl.java
@@ -0,0 +1,108 @@
+package run.ikaros.server.core.subject;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.stream.Collectors;
+import org.springframework.stereotype.Service;
+import org.springframework.util.Assert;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import run.ikaros.server.store.entity.SubjectRelationEntity;
+import run.ikaros.server.store.enums.SubjectRelationType;
+import run.ikaros.server.store.repository.SubjectRelationRepository;
+
+@Service
+public class SubjectRelationServiceImpl implements SubjectRelationService {
+    private final SubjectRelationRepository subjectRelationRepository;
+
+    public SubjectRelationServiceImpl(SubjectRelationRepository subjectRelationRepository) {
+        this.subjectRelationRepository = subjectRelationRepository;
+    }
+
+
+    @Override
+    public Flux<SubjectRelation> findAllBySubjectId(Long subjectId) {
+        Assert.isTrue(subjectId > 0, "'subjectId' must gt zero.");
+        return subjectRelationRepository.findAllBySubjectId(subjectId)
+            .collectList()
+            .flatMapMany(subjectRelationEntities -> {
+                Map<Integer, SubjectRelation> typeSubjectRelationMap = new HashMap<>();
+                subjectRelationEntities.forEach(subjectRelationEntity -> {
+                    Integer relationType = subjectRelationEntity.getRelationType();
+                    if (typeSubjectRelationMap.containsKey(relationType)) {
+                        SubjectRelation subjectRelation = typeSubjectRelationMap.get(relationType);
+                        subjectRelation.getRelationSubjects()
+                            .add(subjectRelationEntity.getRelationSubjectId());
+                    } else {
+                        var relationSubjectSet = new HashSet<Long>();
+                        relationSubjectSet.add(subjectRelationEntity.getRelationSubjectId());
+                        SubjectRelation subjectRelation = SubjectRelation.builder()
+                            .subject(subjectId)
+                            .relationType(SubjectRelationType.codeOf(relationType))
+                            .relationSubjects(relationSubjectSet)
+                            .build();
+                        typeSubjectRelationMap.put(relationType, subjectRelation);
+                    }
+                });
+                return Flux.fromStream(typeSubjectRelationMap.values().stream());
+            });
+    }
+
+    @Override
+    public Mono<SubjectRelation> findBySubjectIdAndType(Long subjectId,
+                                                        SubjectRelationType relationType) {
+        Assert.isTrue(subjectId > 0, "'subjectId' must gt zero.");
+        Assert.notNull(relationType, "'relationType' must not be null.");
+        return subjectRelationRepository
+            .findAllBySubjectIdAndRelationType(subjectId, relationType.getCode())
+            .map(SubjectRelationEntity::getRelationSubjectId)
+            .collect(Collectors.toSet())
+            .flatMap(relationSubjects -> Mono.just(SubjectRelation.builder()
+                .relationSubjects(relationSubjects)
+                .subject(subjectId)
+                .relationType(relationType)
+                .build()));
+    }
+
+    @Override
+    public Mono<SubjectRelation> createSubjectRelation(SubjectRelation subjectRelation) {
+        Assert.notNull(subjectRelation, "'subjectRelation' must not be null.");
+        Assert.isTrue(subjectRelation.getSubject() > 0, "'subjectRelation' must not be null.");
+        return findBySubjectIdAndType(subjectRelation.getSubject(),
+            subjectRelation.getRelationType())
+            .map(SubjectRelation::getRelationSubjects)
+            .flatMapMany(
+                existsRelationSubjectSet -> Flux.fromStream(subjectRelation.getRelationSubjects()
+                        .stream())
+                    .filter(relationSubject -> !existsRelationSubjectSet.contains(relationSubject))
+                    .flatMap(relationSubject -> Mono.just(SubjectRelationEntity.builder()
+                        .subjectId(subjectRelation.getSubject())
+                        .relationType(subjectRelation.getRelationType().getCode())
+                        .relationSubjectId(relationSubject)
+                        .build()))
+                    .flatMap(subjectRelationRepository::save)
+            )
+            .then(findBySubjectIdAndType(subjectRelation.getSubject(),
+                subjectRelation.getRelationType()));
+    }
+
+    @Override
+    public Mono<SubjectRelation> removeSubjectRelation(SubjectRelation subjectRelation) {
+        Assert.notNull(subjectRelation, "'subjectRelation' must not be null.");
+        Assert.isTrue(subjectRelation.getSubject() > 0, "'subjectRelation' must not be null.");
+        return findBySubjectIdAndType(subjectRelation.getSubject(),
+            subjectRelation.getRelationType())
+            .map(SubjectRelation::getRelationSubjects)
+            .flatMapMany(existsRelationSubjectSet
+                -> Flux.fromStream(subjectRelation.getRelationSubjects().stream())
+                .filter(existsRelationSubjectSet::contains)
+                .flatMap(relationSubject
+                    -> subjectRelationRepository
+                    .deleteBySubjectIdAndRelationTypeAndRelationSubjectId(
+                        subjectRelation.getSubject(),
+                        subjectRelation.getRelationType().getCode(), relationSubject))
+            )
+            .then(Mono.just(subjectRelation));
+    }
+}
