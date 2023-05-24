114a43b88aeb69c4ca89e07370a9f4c3b9cff017
li-guohao <git@liguohao.cn>
2023-05-24 20:17:25 +0800

    install element plus in console

--- console/src/components/HelloWorld.vue
+++ console/src/components/HelloWorld.vue
@@ -10,7 +10,8 @@ const count = ref(0)
     <h1>{{ msg }}</h1>
 
     <div class="card">
-        <button type="button" @click="count++">count is {{ count }}</button>
+        <el-button round @click="count++">count is {{ count }}</el-button>
+        <!-- <button type="button" ></button> -->
         <p>
             Edit
             <code>components/HelloWorld.vue</code> to test HMR
--- console/vite.config.ts
+++ console/vite.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()],
+    }),
+  ],
 })
