8b014e66a2f47411c84e5b6f1ad13fdb015faa2c
li-guohao <git@liguohao.cn>
2023-05-25 11:46:54 +0800

    Update HelloWorld component

--- console/src/components/HelloWorld.vue
+++ console/src/components/HelloWorld.vue
@@ -10,27 +10,14 @@ const count = ref(0)
     <h1>{{ msg }}</h1>
 
     <div class="card">
-        <el-button round @click="count++">count is {{ count }}</el-button>
-        <!-- <button type="button" ></button> -->
+        <p>Hello Ikaros</p>
+        <el-button round  @click="count++">count is {{ count }}</el-button>
         <p>
             Edit
             <code>components/HelloWorld.vue</code> to test HMR
         </p>
-        <p>Hello Ikaros</p>
     </div>
 
-    <p>
-        Check out
-        <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-        >create-vue</a
-        >, the official Vue + Vite starter
-    </p>
-    <p>
-        Install
-        <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
-        in your IDE for a better DX
-    </p>
-    <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
 </template>
 
 <style scoped>
