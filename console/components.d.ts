0cb5243de12b38ac2d19b78f5fe0db9a90f6d722
li-guohao <git@liguohao.cn>
2023-05-25 13:06:03 +0800

    Add some dependencies

--- console/components.d.ts
+++ console/components.d.ts
@@ -11,5 +11,7 @@ declare module '@vue/runtime-core' {
   export interface GlobalComponents {
     ElButton: typeof import('element-plus/es')['ElButton']
     HelloWorld: typeof import('./src/components/HelloWorld.vue')['default']
+    RouterLink: typeof import('vue-router')['RouterLink']
+    RouterView: typeof import('vue-router')['RouterView']
   }
 }
