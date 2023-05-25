dfe53dd4ccdb64a08cb99cdf09933353de799e25
li-guohao <git@liguohao.cn>
2023-05-25 10:58:18 +0800

    Update ikaros console logo favicon ico

--- console/src/App.vue
+++ console/src/App.vue
@@ -4,14 +4,11 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <template>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    <a href="https://ikaros.run" target="_blank">
+      <img src="/favicon.ico" class="logo" alt="Ikaros logo" />
     </a>
   </div>
-  <HelloWorld msg="Vite + Vue" />
+  <HelloWorld msg="Ikaros Console" />
 </template>
 
 <style scoped>
