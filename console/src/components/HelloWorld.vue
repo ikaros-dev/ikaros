e4f2e8da9477a120426f9b02e6c1db57fcae09d0
li-guohao <46225881+li-guohao@users.noreply.github.com>
2023-05-25 12:35:16 +0800

    Console module dev start (#303)
    * install element plus in console
    
    * Update ikaros console logo favicon ico
    
    * update github workflow ci config that igonre console paths
    
    * Fix github workflow ci can not work normally
    
    * Add head meta tag in index html
    
    * Update and rename ikaros.yml to ikaros-server-ci.yml
    
    update paths for workflow
    
    Signed-off-by: li-guohao <46225881+li-guohao@users.noreply.github.com>
    
    * Remove default folder id in FileConst
    
    * Update HelloWorld component
    
    * Update ikaros-server-ci.yml
    
    Signed-off-by: li-guohao <46225881+li-guohao@users.noreply.github.com>
    
    * Update ikaros-server-ci.yml
    
    * Remove style tag
    
    ---------
    
    Signed-off-by: li-guohao <46225881+li-guohao@users.noreply.github.com>

--- console/src/components/HelloWorld.vue
+++ console/src/components/HelloWorld.vue
@@ -10,30 +10,12 @@ const count = ref(0)
     <h1>{{ msg }}</h1>
 
     <div class="card">
-        <button type="button" @click="count++">count is {{ count }}</button>
+        <p>Hello Ikaros</p>
+        <el-button round  @click="count++">count is {{ count }}</el-button>
         <p>
             Edit
             <code>components/HelloWorld.vue</code> to test HMR
         </p>
-        <p>Hello Ikaros</p>
     </div>
 
-    <p>
-        Check out
-        <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-        >create-vue</a
-        >, the official Vue + Vite starter
-    </p>
-    <p>
-        Install
-        <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
-        in your IDE for a better DX
-    </p>
-    <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
 </template>
-
-<style scoped>
-.read-the-docs {
-    color: #888;
-}
-</style>
