2a009282b384deb6d9cfae880e09222e09f98887
li-guohao <git@liguohao.cn>
2023-05-25 20:58:10 +0800

    build: add console stylelint hook

--- console/src/App.vue
+++ console/src/App.vue
@@ -18,9 +18,11 @@ import HelloWorld from './components/HelloWorld.vue';
 	will-change: filter;
 	transition: filter 300ms;
 }
+
 .logo:hover {
 	filter: drop-shadow(0 0 2em #646cffaa);
 }
+
 .logo.vue:hover {
 	filter: drop-shadow(0 0 2em #42b883aa);
 }
