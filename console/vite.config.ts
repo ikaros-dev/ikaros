8fec2c196e10a35a884640ee93697d7a74c5ed99
li-guohao <git@liguohao.cn>
2023-05-26 12:14:22 +0800

    build: add element plus auto import dev dependency

--- console/vite.config.ts
+++ console/vite.config.ts
@@ -5,13 +5,32 @@ import { fileURLToPath, URL } from 'url';
 import Compression from 'vite-plugin-compression2';
 import eslintPlugin from 'vite-plugin-eslint';
 import stylelitPlugin from 'vite-plugin-stylelint';
+// ElementPlus
+// eslint-disable-next-line import/no-unresolved
+import AutoImport from 'unplugin-auto-import/vite';
+// eslint-disable-next-line import/no-unresolved
+import Components from 'unplugin-vue-components/vite';
+// eslint-disable-next-line import/no-unresolved
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers';
 
 export default ({ mode }: { mode: string }) => {
 	const env = loadEnv(mode, process.cwd(), '');
 
 	return defineConfig({
 		base: env.VITE_BASE_URL,
-		plugins: [vue(), VueJsx(), eslintPlugin(), Compression(), stylelitPlugin()],
+		plugins: [
+			vue(),
+			VueJsx(),
+			eslintPlugin(),
+			Compression(),
+			stylelitPlugin(),
+			AutoImport({
+				resolvers: [ElementPlusResolver()],
+			}),
+			Components({
+				resolvers: [ElementPlusResolver()],
+			}),
+		],
 		server: {
 			port: 3000,
 		},
