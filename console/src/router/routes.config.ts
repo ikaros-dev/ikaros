545971177500ccbeb4e872e2eec5c928f49ef344
li-guohao <git@liguohao.cn>
2023-05-29 14:48:42 +0800

    build: @ alias path config

--- console/src/router/routes.config.ts
+++ console/src/router/routes.config.ts
@@ -1,10 +1,10 @@
-import BasicLayout from '../layouts/BasicLayout.vue';
+import BasicLayout from '@/layouts/BasicLayout.vue';
 import type { RouteRecordRaw } from 'vue-router';
 
 export const routes: Array<RouteRecordRaw> = [
 	{
 		path: '/hello',
-		component: () => import('../components/HelloWorld.vue'),
+		component: () => import('@/components/HelloWorld.vue'),
 	},
 	{
 		path: '/',
