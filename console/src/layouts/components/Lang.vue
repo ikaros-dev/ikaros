c0ebbbb4b39100bfe535b110c69357314a61e39b
li-guohao <git@liguohao.cn>
2023-05-31 13:52:07 +0800

    feat: add hiht when click switch english dropdown item

--- console/src/layouts/components/Lang.vue
+++ console/src/layouts/components/Lang.vue
@@ -3,6 +3,9 @@ const currentLanguage = 'zh';
 const handleCommand = (val) => {
 	console.log('command', val);
 };
+const hiht = () => {
+	alert('暂时不支持国际化，无法进行中英文切换，请等待后续版本更新。');
+};
 </script>
 
 <template>
@@ -13,7 +16,10 @@ const handleCommand = (val) => {
 				<el-dropdown-item command="zh" :disabled="currentLanguage === 'zh'"
 					>中文</el-dropdown-item
 				>
-				<el-dropdown-item command="en" :disabled="currentLanguage === 'en'"
+				<el-dropdown-item
+					command="en"
+					:disabled="currentLanguage === 'en'"
+					@click="hiht"
 					>English</el-dropdown-item
 				>
 			</el-dropdown-menu>
