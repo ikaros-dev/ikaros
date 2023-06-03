1d172733c5a35c621016757b1aff0f2279a3c2ac
li-guohao <git@liguohao.cn>
2023-06-03 18:29:38 +0800

    fix: can not load i18n yaml file

--- console/vite.config.ts
+++ console/vite.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig, loadEnv } from 'vite';
+import path from 'path';
 import vue from '@vitejs/plugin-vue';
 import VueJsx from '@vitejs/plugin-vue-jsx';
+import VueI18nPlugin from '@intlify/unplugin-vue-i18n/vite';
 import { fileURLToPath, URL } from 'url';
 import Compression from 'vite-plugin-compression2';
 import eslintPlugin from 'vite-plugin-eslint';
@@ -28,6 +30,9 @@ export default ({ mode }: { mode: string }) => {
 			}),
 			Components({
 				resolvers: [ElementPlusResolver()],
+			}),
+			VueI18nPlugin({
+				include: [path.resolve(__dirname, './src/locales/*.yaml')],
 			}),
 		],
 		server: {
