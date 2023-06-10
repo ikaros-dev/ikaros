61fc2a4928ef1c3e72610d868a9be3332a841709
li-guohao <git@liguohao.cn>
2023-06-11 01:10:56 +0800

    update: add setting page.

--- console/src/modules/index.ts
+++ console/src/modules/index.ts
@@ -3,7 +3,8 @@ import User from './system/user/module';
 import File from './content/file/module';
 import Subject from './content/subject/module';
 import Plugin from './system/plugin/module';
+import Setting from './system/setting/module';
 
-const coreModules = [Dashboard, User, File, Subject, Plugin];
+const coreModules = [Dashboard, User, File, Subject, Plugin, Setting];
 
 export { coreModules };
---
+++ console/src/modules/system/setting/Setting.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts"></script>
+
+<template>
+	<div>
+		<el-tabs type="border-card">
+			<el-tab-pane label="User">User</el-tab-pane>
+			<el-tab-pane label="Config">Config</el-tab-pane>
+			<el-tab-pane label="Role">Role</el-tab-pane>
+			<el-tab-pane label="Task">Task</el-tab-pane>
+		</el-tabs>
+	</div>
+</template>
+
+<style lang="scss" scoped></style>
