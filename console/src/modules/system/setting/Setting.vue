dabb0678dff8960e308ab074d14b3224efd1325e
li-guohao <git@liguohao.cn>
2023-06-11 17:22:20 +0800

    build: gen new api-client@0.0.11 and publish to npm center repo in @runikaros/api-client

--- console/src/modules/system/setting/Setting.vue
+++ console/src/modules/system/setting/Setting.vue
@@ -1,12 +1,37 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const setting = ref({
+	SITE_TITLE: '',
+	SITE_SUBHEAD: '',
+	LOGO: '',
+	FAVICON: '',
+	ALLOW_REGISTER: 'false',
+	DEFAULT_ROLE: 'false',
+	GLOBAL_HEADER: '',
+	GLOBAL_FOOTER: '',
+});
+</script>
 
 <template>
 	<div>
 		<el-tabs type="border-card">
-			<el-tab-pane label="User">User</el-tab-pane>
-			<el-tab-pane label="Config">Config</el-tab-pane>
-			<el-tab-pane label="Role">Role</el-tab-pane>
-			<el-tab-pane label="Task">Task</el-tab-pane>
+			<el-tab-pane label="基础设置">
+				<el-input
+					v-model="setting.SITE_TITLE"
+					placeholder="请输入站点标题"
+					style="width: 512"
+					clearable
+				/>
+				<el-input
+					v-model="setting.SITE_SUBHEAD"
+					placeholder="请输入站点副标题"
+					style="width: 512"
+					clearable
+				/>
+			</el-tab-pane>
+			<el-tab-pane label="用户设置">
+				<h1>用户设置</h1>
+			</el-tab-pane>
+			<el-tab-pane label="代码注入">Config</el-tab-pane>
 		</el-tabs>
 	</div>
 </template>
