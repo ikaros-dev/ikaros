cefaba8d1c4165ac588a50f4a49aac137d55352d
li-guohao <git@liguohao.cn>
2023-06-12 23:53:21 +0800

    fix: git ignore incorrect.

---
+++ console/src/components/upload/UppyUpload.vue
@@ -0,0 +1,115 @@
+<script lang="ts" setup>
+import { Dashboard } from '@uppy/vue';
+import '@uppy/core/dist/style.css';
+import '@uppy/dashboard/dist/style.css';
+import Uppy, { type SuccessResponse } from '@uppy/core';
+import type { Restrictions } from '@uppy/core';
+import XHRUpload from '@uppy/xhr-upload';
+import zh_CN from '@uppy/locales/lib/zh_CN';
+import zh_TW from '@uppy/locales/lib/zh_TW';
+import en_US from '@uppy/locales/lib/en_US';
+import { computed, onUnmounted } from 'vue';
+import type { ProblemDetail } from '@/utils/api-client';
+import { i18n } from '@/locales';
+import { ElMessage } from 'element-plus';
+
+const props = withDefaults(
+	defineProps<{
+		restrictions?: Restrictions;
+		meta?: Record<string, unknown>;
+		autoProceed?: boolean;
+		allowedMetaFields?: string[];
+		endpoint: string;
+		name?: string;
+		note?: string;
+		method?: 'GET' | 'POST' | 'PUT' | 'HEAD' | 'get' | 'post' | 'put' | 'head';
+		disabled?: boolean;
+		doneButtonHandler?: () => void;
+	}>(),
+	{
+		restrictions: undefined,
+		meta: undefined,
+		autoProceed: false,
+		allowedMetaFields: undefined,
+		name: 'file',
+		note: undefined,
+		method: 'post',
+		disabled: false,
+		doneButtonHandler: undefined,
+	}
+);
+
+const emit = defineEmits<{
+	// eslint-disable-next-line no-unused-vars
+	(event: 'uploaded', response: SuccessResponse): void;
+	// eslint-disable-next-line no-unused-vars
+	(event: 'error', file, response): void;
+}>();
+
+const locales = {
+	en: en_US,
+	zh: zh_CN,
+	'en-US': en_US,
+	'zh-CN': zh_CN,
+	'zh-TW': zh_TW,
+};
+
+const uppy = computed(() => {
+	return new Uppy({
+		locale: locales[i18n.global.locale.value] || locales['zh-CN'],
+		meta: props.meta,
+		restrictions: props.restrictions,
+		autoProceed: props.autoProceed,
+	}).use(XHRUpload, {
+		endpoint: `${import.meta.env.VITE_API_URL}${props.endpoint}`,
+		allowedMetaFields: props.allowedMetaFields,
+		withCredentials: true,
+		formData: true,
+		fieldName: props.name,
+		method: props.method,
+		limit: 5,
+		timeout: 0,
+		getResponseError: (responseText) => {
+			const response = JSON.parse(responseText);
+			const { title, detail } = (response || {}) as ProblemDetail;
+			const message = [title, detail].filter(Boolean).join(': ');
+
+			if (message) {
+				ElMessage.error({
+					message: message,
+					duration: 5000,
+				});
+
+				return new Error(message);
+			}
+			return new Error('Internal Server Error');
+		},
+	});
+});
+
+uppy.value.on('upload-success', (_, response: SuccessResponse) => {
+	emit('uploaded', response);
+});
+
+uppy.value.on('upload-error', (file, _, response) => {
+	emit('error', file, response);
+});
+
+onUnmounted(() => {
+	uppy.value.close({ reason: 'unmount' });
+});
+</script>
+
+<template>
+	<dashboard
+		:uppy="uppy"
+		:props="{
+			theme: 'light',
+			disabled: disabled,
+			inline: true,
+			width: '100%',
+			note: note,
+			doneButtonHandler: doneButtonHandler,
+		}"
+	/>
+</template>
