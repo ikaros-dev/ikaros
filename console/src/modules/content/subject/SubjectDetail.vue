ce856e2858b8b089a63590c42ee2f45be78ee9f9
li-guohao <git@liguohao.cn>
2023-06-14 22:19:27 +0800

    feat: add some subject pages.

---
+++ console/src/modules/content/subject/SubjectDetail.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+	<div></div>
+</template>
+
+<style lang="scss" scoped></style>
--- console/src/modules/content/subject/Subjects.vue
+++ console/src/modules/content/subject/Subjects.vue
@@ -7,17 +7,17 @@ const findSubjectsCondition = ref({
 	total: 10,
 	name: '',
 	nameCn: '',
-	nsfw: true,
+	nsfw: undefined,
 	type: undefined,
 });
 </script>
 
 <template>
 	<el-row :gutter="10">
-		<el-col :xs="24" :sm="24" :md="24" :lg="14" :xl="14">
+		<el-col :xs="24" :sm="24" :md="24" :lg="20" :xl="20">
 			<el-form :inline="true" :model="findSubjectsCondition">
 				<el-row :gutter="1">
-					<el-col :xs="24" :sm="24" :md="24" :lg="6" :xl="6">
+					<el-col :xs="24" :sm="24" :md="24" :lg="7" :xl="7">
 						<el-form-item label="原始名称" style="width: 95%">
 							<el-input
 								v-model="findSubjectsCondition.name"
@@ -26,7 +26,7 @@ const findSubjectsCondition = ref({
 							/>
 						</el-form-item>
 					</el-col>
-					<el-col :xs="24" :sm="24" :md="24" :lg="6" :xl="6">
+					<el-col :xs="24" :sm="24" :md="24" :lg="7" :xl="7">
 						<el-form-item label="中文名称" style="width: 95%">
 							<el-input
 								v-model="findSubjectsCondition.nameCn"
@@ -35,22 +35,17 @@ const findSubjectsCondition = ref({
 							/>
 						</el-form-item>
 					</el-col>
-					<el-col :xs="24" :sm="24" :md="24" :lg="6" :xl="6">
+					<el-col :xs="24" :sm="24" :md="24" :lg="5" :xl="5">
 						<el-form-item label="NSFW" style="width: 95%">
-							<el-switch
-								v-model="findSubjectsCondition.nsfw"
-								active-text="打开"
-								inactive-text="关闭"
-							/>
+							<el-select v-model="findSubjectsCondition.nsfw" clearable>
+								<el-option label="是" value="true" />
+								<el-option label="非" value="false" />
+							</el-select>
 						</el-form-item>
 					</el-col>
-					<el-col :xs="24" :sm="24" :md="24" :lg="6" :xl="6">
+					<el-col :xs="24" :sm="24" :md="24" :lg="5" :xl="5">
 						<el-form-item label="所属类型" style="width: 95%">
-							<el-select
-								v-model="findSubjectsCondition.type"
-								clearable
-								style="width: 90px"
-							>
+							<el-select v-model="findSubjectsCondition.type" clearable>
 								<el-option label="动漫" value="ANIME" />
 								<el-option label="漫画" value="COMIC" />
 								<el-option label="游戏" value="GAME" />
@@ -65,22 +60,12 @@ const findSubjectsCondition = ref({
 			</el-form>
 		</el-col>
 
-		<el-col :xs="24" :sm="24" :md="24" :lg="8" :xl="8">
-			<el-pagination
-				v-model:page-size="findSubjectsCondition.size"
-				v-model:current-page="findSubjectsCondition.page"
-				background
-				:total="findSubjectsCondition.total"
-				layout="total, sizes, prev, pager, next, jumper"
-			/>
-		</el-col>
-
 		<el-col
 			:xs="24"
 			:sm="24"
 			:md="24"
-			:lg="2"
-			:xl="2"
+			:lg="4"
+			:xl="4"
 			style="text-align: right"
 		>
 			<el-button plain> 新建条目 </el-button>
@@ -112,6 +97,17 @@ const findSubjectsCondition = ref({
 		<el-col :xs="24" :sm="12" :md="8" :lg="4" :xl="4">
 			<SubjectCard />
 		</el-col>
+	</el-row>
+
+	<el-row>
+		<el-col
+			><el-pagination
+				v-model:page-size="findSubjectsCondition.size"
+				v-model:current-page="findSubjectsCondition.page"
+				background
+				:total="findSubjectsCondition.total"
+				layout="total, sizes, prev, pager, next, jumper"
+		/></el-col>
 	</el-row>
 </template>
 
