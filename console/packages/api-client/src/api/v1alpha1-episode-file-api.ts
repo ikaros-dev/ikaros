b1a3d465b92fd632797b05e79ef48d49c3fe0625
li-guohao <git@liguohao.cn>
2023-06-20 17:15:59 +0800

    build: gen new api-client@0.0.24 and publish to npm center repo in @runikaros/api-client

--- console/packages/api-client/src/api/v1alpha1-episode-file-api.ts
+++ console/packages/api-client/src/api/v1alpha1-episode-file-api.ts
@@ -37,6 +37,8 @@ import {
 	BaseAPI,
 	RequiredError,
 } from '../base';
+// @ts-ignore
+import { BatchMatchingEpisodeFile } from '../models';
 /**
  * V1alpha1EpisodeFileApi - axios parameter creator
  * @export
@@ -45,6 +47,67 @@ export const V1alpha1EpisodeFileApiAxiosParamCreator = function (
 	configuration?: Configuration
 ) {
 	return {
+		/**
+		 * Batch matching episode file.
+		 * @param {BatchMatchingEpisodeFile} batchMatchingEpisodeFile batch matching episode file request value object.
+		 * @param {*} [options] Override http request option.
+		 * @throws {RequiredError}
+		 */
+		batchMatchingEpisodeFile: async (
+			batchMatchingEpisodeFile: BatchMatchingEpisodeFile,
+			options: AxiosRequestConfig = {}
+		): Promise<RequestArgs> => {
+			// verify required parameter 'batchMatchingEpisodeFile' is not null or undefined
+			assertParamExists(
+				'batchMatchingEpisodeFile',
+				'batchMatchingEpisodeFile',
+				batchMatchingEpisodeFile
+			);
+			const localVarPath = `/api/v1alpha1/episodefile/batch`;
+			// use dummy base URL string because the URL constructor only accepts absolute URLs.
+			const localVarUrlObj = new URL(localVarPath, DUMMY_BASE_URL);
+			let baseOptions;
+			if (configuration) {
+				baseOptions = configuration.baseOptions;
+			}
+
+			const localVarRequestOptions = {
+				method: 'POST',
+				...baseOptions,
+				...options,
+			};
+			const localVarHeaderParameter = {} as any;
+			const localVarQueryParameter = {} as any;
+
+			// authentication BasicAuth required
+			// http basic authentication required
+			setBasicAuthToObject(localVarRequestOptions, configuration);
+
+			// authentication BearerAuth required
+			// http bearer authentication required
+			await setBearerAuthToObject(localVarHeaderParameter, configuration);
+
+			localVarHeaderParameter['Content-Type'] = 'application/json';
+
+			setSearchParams(localVarUrlObj, localVarQueryParameter);
+			let headersFromBaseOptions =
+				baseOptions && baseOptions.headers ? baseOptions.headers : {};
+			localVarRequestOptions.headers = {
+				...localVarHeaderParameter,
+				...headersFromBaseOptions,
+				...options.headers,
+			};
+			localVarRequestOptions.data = serializeDataIfNeeded(
+				batchMatchingEpisodeFile,
+				localVarRequestOptions,
+				configuration
+			);
+
+			return {
+				url: toPathString(localVarUrlObj),
+				options: localVarRequestOptions,
+			};
+		},
 		/**
 		 * Create episode and file bind record.
 		 * @param {number} episodeId Episode id.
@@ -171,6 +234,30 @@ export const V1alpha1EpisodeFileApiFp = function (
 		V1alpha1EpisodeFileApiAxiosParamCreator(configuration);
 	return {
 		/**
+		 * Batch matching episode file.
+		 * @param {BatchMatchingEpisodeFile} batchMatchingEpisodeFile batch matching episode file request value object.
+		 * @param {*} [options] Override http request option.
+		 * @throws {RequiredError}
+		 */
+		async batchMatchingEpisodeFile(
+			batchMatchingEpisodeFile: BatchMatchingEpisodeFile,
+			options?: AxiosRequestConfig
+		): Promise<
+			(axios?: AxiosInstance, basePath?: string) => AxiosPromise<void>
+		> {
+			const localVarAxiosArgs =
+				await localVarAxiosParamCreator.batchMatchingEpisodeFile(
+					batchMatchingEpisodeFile,
+					options
+				);
+			return createRequestFunction(
+				localVarAxiosArgs,
+				globalAxios,
+				BASE_PATH,
+				configuration
+			);
+		},
+		/**
 		 * Create episode and file bind record.
 		 * @param {number} episodeId Episode id.
 		 * @param {number} fileId File id
@@ -239,6 +326,23 @@ export const V1alpha1EpisodeFileApiFactory = function (
 	const localVarFp = V1alpha1EpisodeFileApiFp(configuration);
 	return {
 		/**
+		 * Batch matching episode file.
+		 * @param {V1alpha1EpisodeFileApiBatchMatchingEpisodeFileRequest} requestParameters Request parameters.
+		 * @param {*} [options] Override http request option.
+		 * @throws {RequiredError}
+		 */
+		batchMatchingEpisodeFile(
+			requestParameters: V1alpha1EpisodeFileApiBatchMatchingEpisodeFileRequest,
+			options?: AxiosRequestConfig
+		): AxiosPromise<void> {
+			return localVarFp
+				.batchMatchingEpisodeFile(
+					requestParameters.batchMatchingEpisodeFile,
+					options
+				)
+				.then((request) => request(axios, basePath));
+		},
+		/**
 		 * Create episode and file bind record.
 		 * @param {V1alpha1EpisodeFileApiCreateEpisodeFileRequest} requestParameters Request parameters.
 		 * @param {*} [options] Override http request option.
@@ -276,6 +380,20 @@ export const V1alpha1EpisodeFileApiFactory = function (
 		},
 	};
 };
+
+/**
+ * Request parameters for batchMatchingEpisodeFile operation in V1alpha1EpisodeFileApi.
+ * @export
+ * @interface V1alpha1EpisodeFileApiBatchMatchingEpisodeFileRequest
+ */
+export interface V1alpha1EpisodeFileApiBatchMatchingEpisodeFileRequest {
+	/**
+	 * batch matching episode file request value object.
+	 * @type {BatchMatchingEpisodeFile}
+	 * @memberof V1alpha1EpisodeFileApiBatchMatchingEpisodeFile
+	 */
+	readonly batchMatchingEpisodeFile: BatchMatchingEpisodeFile;
+}
 
 /**
  * Request parameters for createEpisodeFile operation in V1alpha1EpisodeFileApi.
@@ -326,6 +444,25 @@ export interface V1alpha1EpisodeFileApiRemoveEpisodeFileRequest {
  * @extends {BaseAPI}
  */
 export class V1alpha1EpisodeFileApi extends BaseAPI {
+	/**
+	 * Batch matching episode file.
+	 * @param {V1alpha1EpisodeFileApiBatchMatchingEpisodeFileRequest} requestParameters Request parameters.
+	 * @param {*} [options] Override http request option.
+	 * @throws {RequiredError}
+	 * @memberof V1alpha1EpisodeFileApi
+	 */
+	public batchMatchingEpisodeFile(
+		requestParameters: V1alpha1EpisodeFileApiBatchMatchingEpisodeFileRequest,
+		options?: AxiosRequestConfig
+	) {
+		return V1alpha1EpisodeFileApiFp(this.configuration)
+			.batchMatchingEpisodeFile(
+				requestParameters.batchMatchingEpisodeFile,
+				options
+			)
+			.then((request) => request(this.axios, this.basePath));
+	}
+
 	/**
 	 * Create episode and file bind record.
 	 * @param {V1alpha1EpisodeFileApiCreateEpisodeFileRequest} requestParameters Request parameters.
