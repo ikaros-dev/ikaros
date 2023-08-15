a8e1f94d26ec623d96a997f25e554160dcbf0558
li-guohao <git@liguohao.cn>
2023-08-15 19:37:20 +0800

    feat: add some java beans for collection module.

---
+++ api/src/main/java/run/ikaros/api/core/collection/EpisodeCollection.java
@@ -0,0 +1,49 @@
+package run.ikaros.api.core.collection;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+import run.ikaros.api.store.enums.EpisodeGroup;
+
+@Data
+@Builder
+@EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
+@Accessors(chain = true)
+public class EpisodeCollection {
+    private Long id;
+    @JsonProperty("user_id")
+    private Long userId;
+    @JsonProperty("episode_id")
+    private Long episodeId;
+    /**
+     * 是否已经看过.
+     */
+    private Boolean finish;
+    /**
+     * 观看进度，时间戳.
+     */
+    private Long progress;
+    /**
+     * 总时长，时间戳.
+     */
+    private Long duration;
+
+    @JsonProperty("subject_id")
+    private Long subjectId;
+    private String name;
+    @JsonProperty("name_cn")
+    private String nameCn;
+    private String description;
+    @JsonProperty("air_time")
+    private LocalDateTime airTime;
+    private Integer sequence;
+    @JsonProperty("ep_group")
+    private EpisodeGroup group;
+}
---
+++ api/src/main/java/run/ikaros/api/core/collection/SubjectCollection.java
@@ -0,0 +1,54 @@
+package run.ikaros.api.core.collection;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+import run.ikaros.api.store.enums.CollectionType;
+import run.ikaros.api.store.enums.SubjectType;
+
+@Data
+@Builder
+@EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
+@Accessors(chain = true)
+public class SubjectCollection {
+    private Long id;
+    @JsonProperty("user_id")
+    private Long userId;
+    @JsonProperty("subject_id")
+    private Long subjectId;
+    private CollectionType type;
+
+    /**
+     * User main group episode watching progress.
+     */
+    @JsonProperty("main_ep_progress")
+    private Integer mainEpisodeProgress;
+
+    /**
+     * Whether it can be accessed without login.
+     */
+    @JsonProperty("is_private")
+    private Boolean isPrivate;
+
+    @JsonProperty("subject_type")
+    private SubjectType subjectType;
+    private String name;
+    @JsonProperty("name_cn")
+    private String nameCn;
+    private String infobox;
+    private String summary;
+    /**
+     * Not Safe/Suitable For Work.
+     */
+    private Boolean nsfw;
+    private String cover;
+    @JsonProperty("air_time")
+    private LocalDateTime airTime;
+}
