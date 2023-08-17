86fea7def602b07817bcd44f4300cc10ba830d3c
li-guohao <git@liguohao.cn>
2023-08-17 16:13:16 +0800

    feat: add collect menu in console

---
+++ console/src/modules/user/Collection.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+	<div>Subject Collection Page</div>
+</template>
+
+<style lang="scss" scoped></style>
--- console/src/modules/user/module.ts
+++ console/src/modules/user/module.ts
@@ -3,7 +3,10 @@ import Login from './Login.vue';
 import Profile from './Profile.vue';
 import { UserFilled } from '@element-plus/icons-vue';
 import { markRaw } from 'vue';
+import Collection from './Collection.vue';
+import { MessageBox } from '@element-plus/icons-vue';
 
+// <el-icon><MessageBox /></el-icon>
 export default definePlugin({
 	name: 'User',
 	components: {},
@@ -32,6 +35,23 @@ export default definePlugin({
 						name: 'core.sidebar.menu.items.profile',
 						group: 'user',
 						icon: markRaw(UserFilled),
+						priority: 1,
+					},
+				},
+			},
+		},
+		{
+			parentName: 'Root',
+			route: {
+				path: '/collection',
+				name: 'Collection',
+				component: Collection,
+				meta: {
+					title: '收藏',
+					menu: {
+						name: '收藏',
+						group: 'user',
+						icon: markRaw(MessageBox),
 						priority: 1,
 					},
 				},
