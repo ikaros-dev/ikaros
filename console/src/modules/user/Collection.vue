6586ddbb3a7dfda2fd675a0b42c381e82432c975
li-guohao <git@liguohao.cn>
2023-08-17 18:35:34 +0800

    feat: add content in Collection.vue

--- console/src/modules/user/Collection.vue
+++ console/src/modules/user/Collection.vue
@@ -1,4 +1,14 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue';
+
+// eslint-disable-next-line no-unused-vars
+const findSubjectCollection = ref({
+	page: 1,
+	size: 12,
+	type: undefined,
+	isPrivate: undefined,
+});
+</script>
 
 <template>
 	<div>Subject Collection Page</div>
