d6484e3e2e23a1a18cea5080a5b34939361b2a65
li-guohao <git@liguohao.cn>
2023-08-21 13:28:29 +0800

    feat: finish collection page

--- console/src/utils/api-client.ts
+++ console/src/utils/api-client.ts
@@ -4,6 +4,7 @@ import type { AxiosError, AxiosInstance } from 'axios';
 import {
 	PluginIkarosRunV1alpha1PluginApi,
 	SettingIkarosRunV1alpha1ConfigMapApi,
+	V1alpha1CollectionSubjectApi,
 	V1alpha1EpisodeFileApi,
 	V1alpha1FileApi,
 	V1alpha1FolderApi,
@@ -161,6 +162,11 @@ function setupApiClient(axios: AxiosInstance) {
 		episodefile: new V1alpha1EpisodeFileApi(undefined, baseURL, axios),
 		indices: new V1alpha1IndicesApi(undefined, baseURL, axios),
 		task: new V1alpha1TaskApi(undefined, baseURL, axios),
+		subjectCollection: new V1alpha1CollectionSubjectApi(
+			undefined,
+			baseURL,
+			axios
+		),
 		// custom endpoints
 		plugin: new PluginIkarosRunV1alpha1PluginApi(undefined, baseURL, axios),
 		configmap: new SettingIkarosRunV1alpha1ConfigMapApi(
