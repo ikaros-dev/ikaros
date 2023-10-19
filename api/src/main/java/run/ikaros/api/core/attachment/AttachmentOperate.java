5f2afe128e0b949cc470cad19c6ae1b8bb4ec51d
li-guohao <git@liguohao.cn>
2023-10-19 18:54:45 +0800

    feat: add AttachmentOperator.java

---
+++ api/src/main/java/run/ikaros/api/core/attachment/AttachmentOperate.java
@@ -0,0 +1,31 @@
+package run.ikaros.api.core.attachment;
+
+import jakarta.annotation.Nonnull;
+import jakarta.annotation.Nullable;
+import jakarta.validation.constraints.NotBlank;
+import reactor.core.publisher.Mono;
+import run.ikaros.api.plugin.AllowPluginOperate;
+import run.ikaros.api.store.enums.AttachmentType;
+import run.ikaros.api.wrap.PagingWrap;
+
+public interface AttachmentOperate extends AllowPluginOperate {
+
+    Mono<Attachment> save(Attachment attachment);
+
+    Mono<PagingWrap<Attachment>> listByCondition(AttachmentSearchCondition searchCondition);
+
+    Mono<Void> removeById(Long attachmentId);
+
+    Mono<Attachment> upload(AttachmentUploadCondition uploadCondition);
+
+    Mono<Attachment> findById(Long attachmentId);
+
+    Mono<Attachment> findByTypeAndParentIdAndName(AttachmentType type, @Nullable Long parentId,
+                                                  String name);
+
+    Mono<Void> removeByTypeAndParentIdAndName(
+        AttachmentType type, @Nullable Long parentId, String name);
+
+
+    Mono<Attachment> createDirectory(@Nullable Long parentId, @NotBlank String name);
+}
