29b344e7be20e1d3e205a3595606b18a1752caed
li-guohao <git@liguohao.cn>
2023-10-24 01:13:20 +0800

    feat: upgrade to v0.11.1 and add attachment ref operate for plugin api.

---
+++ api/src/main/java/run/ikaros/api/core/attachment/AttachmentReferenceOperate.java
@@ -0,0 +1,24 @@
+package run.ikaros.api.core.attachment;
+
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import run.ikaros.api.store.enums.AttachmentReferenceType;
+
+public interface AttachmentReferenceOperate {
+    Mono<AttachmentReference> save(AttachmentReference attachmentReference);
+
+    Flux<AttachmentReference> findAllByTypeAndAttachmentId(
+        AttachmentReferenceType type, Long attachmentId);
+
+    Mono<Void> removeById(Long attachmentRefId);
+
+    Mono<Void> removeByTypeAndAttachmentIdAndReferenceId(
+        AttachmentReferenceType type, Long attachmentId, Long referenceId);
+
+    Mono<Void> matchingAttachmentsAndSubjectEpisodes(Long subjectId, Long[] attachmentIds);
+
+    Mono<Void> matchingAttachmentsAndSubjectEpisodes(Long subjectId, Long[] attachmentIds,
+                                                     boolean notify);
+
+    Mono<Void> matchingAttachmentsForEpisode(Long episodeId, Long[] attachmentIds);
+}
---
+++ server/src/main/java/run/ikaros/server/core/attachment/operator/AttachmentReferenceOperator.java
@@ -0,0 +1,59 @@
+package run.ikaros.server.core.attachment.operator;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import run.ikaros.api.core.attachment.AttachmentReference;
+import run.ikaros.api.core.attachment.AttachmentReferenceOperate;
+import run.ikaros.api.store.enums.AttachmentReferenceType;
+import run.ikaros.server.core.attachment.service.AttachmentReferenceService;
+
+@Slf4j
+@Component
+public class AttachmentReferenceOperator implements AttachmentReferenceOperate {
+    private final AttachmentReferenceService service;
+
+    public AttachmentReferenceOperator(AttachmentReferenceService service) {
+        this.service = service;
+    }
+
+    @Override
+    public Mono<AttachmentReference> save(AttachmentReference attachmentReference) {
+        return service.save(attachmentReference);
+    }
+
+    @Override
+    public Flux<AttachmentReference> findAllByTypeAndAttachmentId(AttachmentReferenceType type,
+                                                                  Long attachmentId) {
+        return service.findAllByTypeAndAttachmentId(type, attachmentId);
+    }
+
+    @Override
+    public Mono<Void> removeById(Long attachmentRefId) {
+        return service.removeById(attachmentRefId);
+    }
+
+    @Override
+    public Mono<Void> removeByTypeAndAttachmentIdAndReferenceId(AttachmentReferenceType type,
+                                                                Long attachmentId,
+                                                                Long referenceId) {
+        return service.removeByTypeAndAttachmentIdAndReferenceId(type, attachmentId, referenceId);
+    }
+
+    @Override
+    public Mono<Void> matchingAttachmentsAndSubjectEpisodes(Long subjectId, Long[] attachmentIds) {
+        return service.matchingAttachmentsAndSubjectEpisodes(subjectId, attachmentIds);
+    }
+
+    @Override
+    public Mono<Void> matchingAttachmentsAndSubjectEpisodes(Long subjectId, Long[] attachmentIds,
+                                                            boolean notify) {
+        return service.matchingAttachmentsAndSubjectEpisodes(subjectId, attachmentIds, notify);
+    }
+
+    @Override
+    public Mono<Void> matchingAttachmentsForEpisode(Long episodeId, Long[] attachmentIds) {
+        return service.matchingAttachmentsForEpisode(episodeId, attachmentIds);
+    }
+}
