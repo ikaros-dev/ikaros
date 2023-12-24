711413ff16d55dd28d19b1295004b719015345e3
li-guohao <git@liguohao.cn>
2023-12-24 17:24:56 +0800

    feat: add episode endpoints.

---
+++ api/src/main/java/run/ikaros/api/core/subject/EpisodeMeta.java
@@ -0,0 +1,30 @@
+package run.ikaros.api.core.subject;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import java.time.LocalDateTime;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+import run.ikaros.api.store.enums.EpisodeGroup;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Accessors(chain = true)
+public class EpisodeMeta {
+    private Long id;
+    @JsonProperty("subject_id")
+    private Long subjectId;
+    private String name;
+    @JsonProperty("name_cn")
+    private String nameCn;
+    private String description;
+    @JsonProperty("air_time")
+    private LocalDateTime airTime;
+    private Integer sequence;
+    private EpisodeGroup group;
+}
---
+++ server/src/main/java/run/ikaros/server/core/episode/DefaultEpisodeService.java
@@ -0,0 +1,71 @@
+package run.ikaros.server.core.episode;
+
+import static run.ikaros.server.infra.utils.ReactiveBeanUtils.copyProperties;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.util.Assert;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import run.ikaros.api.core.subject.Episode;
+import run.ikaros.api.core.subject.EpisodeMeta;
+import run.ikaros.api.core.subject.EpisodeResource;
+import run.ikaros.api.store.enums.AttachmentReferenceType;
+import run.ikaros.server.store.repository.AttachmentReferenceRepository;
+import run.ikaros.server.store.repository.AttachmentRepository;
+import run.ikaros.server.store.repository.EpisodeRepository;
+
+@Slf4j
+@Service
+public class DefaultEpisodeService implements EpisodeService {
+    private final EpisodeRepository episodeRepository;
+    private final AttachmentReferenceRepository attachmentReferenceRepository;
+    private final AttachmentRepository attachmentRepository;
+
+    /**
+     * Construct.
+     */
+    public DefaultEpisodeService(EpisodeRepository episodeRepository,
+                                 AttachmentReferenceRepository attachmentReferenceRepository,
+                                 AttachmentRepository attachmentRepository) {
+        this.episodeRepository = episodeRepository;
+        this.attachmentReferenceRepository = attachmentReferenceRepository;
+        this.attachmentRepository = attachmentRepository;
+    }
+
+    @Override
+    public Mono<EpisodeMeta> findMetaById(Long episodeId) {
+        Assert.isTrue(episodeId >= 0, "'episodeId' must >= 0.");
+        return episodeRepository.findById(episodeId)
+            .flatMap(episodeEntity -> copyProperties(episodeEntity, new EpisodeMeta()));
+    }
+
+    @Override
+    public Mono<Episode> findById(Long episodeId) {
+        Assert.isTrue(episodeId >= 0, "'episodeId' must >= 0.");
+        return episodeRepository.findById(episodeId)
+            .flatMap(episodeEntity -> copyProperties(episodeEntity, new Episode()))
+            .flatMap(episode ->
+                findResourcesById(episodeId).collectList()
+                    .map(episode::setResources));
+    }
+
+    @Override
+    public Flux<EpisodeResource> findResourcesById(Long episodeId) {
+        Assert.isTrue(episodeId >= 0, "'episodeId' must >= 0.");
+        return attachmentReferenceRepository
+            .findAllByTypeAndReferenceIdOrderByTypeAscAttachmentIdAsc(
+                AttachmentReferenceType.EPISODE, episodeId)
+            .flatMap(attachmentReferenceEntity ->
+                attachmentRepository.findById(attachmentReferenceEntity.getAttachmentId())
+                    .map(attachmentEntity -> EpisodeResource.builder()
+                        .episodeId(episodeId)
+                        .attachmentId(attachmentEntity.getId())
+                        .parentAttachmentId(attachmentEntity.getParentId())
+                        .name(attachmentEntity.getName())
+                        .url(attachmentEntity.getUrl())
+                        .canRead(true)
+                        .build())
+            );
+    }
+}
