1e989135ac72f8117400de794a9b10ae439cb503
li-guohao <git@liguohao.cn>
2024-06-07 12:22:14 +0800

    feat: remember i18n select in console page.

--- console/src/stores/layout.ts
+++ console/src/stores/layout.ts
@@ -3,12 +3,14 @@ import { defineStore } from 'pinia';
 interface LayoutStoreState {
 	asideIsExtend: boolean;
 	currentActivePath: string;
+	i18nCode: string;
 }
 
 export const useLayoutStore = defineStore('layout', {
 	state: (): LayoutStoreState => ({
 		asideIsExtend: true,
 		currentActivePath: '/dashboard',
+		i18nCode: 'zh-CN',
 	}),
 	actions: {
 		switchLayoutAsideExtendState() {
@@ -16,6 +18,9 @@ export const useLayoutStore = defineStore('layout', {
 		},
 		updatecurrentActivePathByRoutePath(path) {
 			this.currentActivePath = path;
+		},
+		setI18nCode(code:string) {
+			this.i18nCode = code;
 		},
 	},
 	// In order to config pinia-plugin-persist, please see https://github.com/Seb-L/pinia-plugin-persist
