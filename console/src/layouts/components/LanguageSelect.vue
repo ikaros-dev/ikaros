8f38b5fa17a0d8a87592cd0f2a0ce6e3ecff5607
li-guohao <git@liguohao.cn>
2024-06-07 12:26:00 +0800

    feat: remember i18n select in console page. (#550)
    * fix: default dev mode can not run success.
    
    * feat: remember i18n select in console page.
    
    * docs: update CHANGELOG.MD

--- console/src/layouts/components/LanguageSelect.vue
+++ console/src/layouts/components/LanguageSelect.vue
@@ -1,15 +1,27 @@
 <script setup lang="ts">
 import { useI18n } from 'vue-i18n';
 import { ElSelect, ElOption } from 'element-plus';
-import { ref } from 'vue';
+import { onMounted, ref } from 'vue';
 import { locales, changeI18nLocal } from '@/locales';
+import { useLayoutStore } from '@/stores/layout';
 
 const { locale } = useI18n();
 const selectedLanguage = ref(locale);
+const layoutStore = useLayoutStore();
 
 const changeLanguage = () => {
 	changeI18nLocal(selectedLanguage.value);
+	// console.debug('selectedLanguage.value', selectedLanguage.value);
+	layoutStore.setI18nCode(selectedLanguage.value);
 };
+onMounted(()=>{
+	const selectdI18nCode = layoutStore.i18nCode
+	if (selectdI18nCode && selectdI18nCode != '') {
+		// console.debug('selectdI18nCode', selectdI18nCode)
+		selectedLanguage.value = selectdI18nCode;
+		changeI18nLocal(selectedLanguage.value);
+	}
+})
 </script>
 
 <template>
