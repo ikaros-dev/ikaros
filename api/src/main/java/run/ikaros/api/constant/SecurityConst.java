81f08b1a80393091524ad9b229458f5df3005e83
chivehao <git@liguohao.cn>
2024-06-26 04:46:28 +0800

    feat: add some const for auth in SecurityConst.java

--- api/src/main/java/run/ikaros/api/constant/SecurityConst.java
+++ api/src/main/java/run/ikaros/api/constant/SecurityConst.java
@@ -1,5 +1,9 @@
 package run.ikaros.api.constant;
 
+import static run.ikaros.api.constant.OpenApiConst.CORE_VERSION;
+
+import org.springframework.http.HttpMethod;
+
 /**
  * security constants.
  *
@@ -32,6 +36,47 @@ public interface SecurityConst {
 
         static boolean isAnonymousUser(String principal) {
             return PRINCIPAL.equals(principal);
+        }
+    }
+
+    interface Authorization {
+        interface Target {
+            String ALL = "*";
+            String API_CORE_USER = "/api/" + CORE_VERSION + "/user/**";
+            String API_CORE_STATIC = "/api/" + CORE_VERSION + "/static/**";
+            String API_CORE_ATTACHMENT = "/api/" + CORE_VERSION + "/attachment/**";
+            String API_CORE_SUBJECT = "/api/" + CORE_VERSION + "/subject/**";
+            String API_CORE_TAG = "/api/" + CORE_VERSION + "/tag/**";
+            String API_CORE_TASK = "/api/" + CORE_VERSION + "/task/**";
+            String API_CORE_PLUGIN = "/api/" + CORE_VERSION + "/plugin/**";
+            String API_CORE_INDICES = "/api/" + CORE_VERSION + "/indices/**";
+            String API_CORE_EPISODE = "/api/" + CORE_VERSION + "/episode/**";
+            String APIS_CUSTOM = "/apis/**";
+            String MENU_DASHBOARD = "/dashboard/**";
+            String MENU_ATTACHMENTS = "/attachments/**";
+            String MENU_SUBJECTS = "/subjects/**";
+            String MENU_PROFILE = "/profile/**";
+            String MENU_COLLECTION = "/collection/**";
+            String MENU_SETTING = "/setting/**";
+            String MENU_ROLES = "/roles/**";
+            String MENU_USERS = "/users/**";
+            String MENU_PLUGINS = "/plugins/**";
+            String MENU_ABOUT = "/about/**";
+            String URL_CONSOLE = "/console/**";
+            String URL_BASIC = "/**";
+        }
+
+        interface Authority {
+            String ALL = "*";
+            String HTTP_ALL = "HTTP_**";
+            String HTTP_GET = "HTTP_" + HttpMethod.GET.name();
+            String HTTP_HEAD = "HTTP_" + HttpMethod.HEAD.name();
+            String HTTP_POST = "HTTP_" + HttpMethod.POST.name();
+            String HTTP_PUT = "HTTP_" + HttpMethod.PUT.name();
+            String HTTP_PATCH = "HTTP_" + HttpMethod.PATCH.name();
+            String HTTP_DELETE = "HTTP_" + HttpMethod.DELETE.name();
+            String HTTP_OPTIONS = "HTTP_" + HttpMethod.OPTIONS.name();
+            String HTTP_TRACE = "HTTP_" + HttpMethod.TRACE.name();
         }
     }
 }
--- api/src/main/java/run/ikaros/api/store/enums/AuthorityType.java
+++ api/src/main/java/run/ikaros/api/store/enums/AuthorityType.java
@@ -3,7 +3,7 @@ package run.ikaros.api.store.enums;
 public enum AuthorityType {
     ALL,
     API,
-    CONSOLE_MENU,
-    CONSOLE_UI_COMPONENT,
+    APIS,
+    MENU,
     URL
 }
