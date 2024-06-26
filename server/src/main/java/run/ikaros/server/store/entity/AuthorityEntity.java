2b95d8b1eba072204e5442fc04065bee9d63dd6f
chivehao <git@liguohao.cn>
2024-06-26 13:22:57 +0800

    optimize: 精简权限表字段

--- server/src/main/java/run/ikaros/server/store/entity/AuthorityEntity.java
+++ server/src/main/java/run/ikaros/server/store/entity/AuthorityEntity.java
@@ -6,7 +6,6 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import lombok.experimental.Accessors;
-import org.springframework.data.relational.core.mapping.Column;
 import org.springframework.data.relational.core.mapping.Table;
 import run.ikaros.api.store.enums.AuthorityType;
 
@@ -18,8 +17,6 @@ import run.ikaros.api.store.enums.AuthorityType;
 @Accessors(chain = true)
 @EqualsAndHashCode(callSuper = true)
 public class AuthorityEntity extends BaseEntity {
-    @Column("role_id")
-    private Long roleId;
     private Boolean allow;
     private AuthorityType type;
     private String target;
