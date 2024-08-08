299b8b2a30e700a2cb18936b0c72741f666e46ba
ChiveHao <git@liguohao.cn>
2024-08-08 10:55:00 +0800

    fix: basic auth issue that not set Authentication in SecurityContextHolder (#643)

--- server/src/main/java/run/ikaros/server/security/authentication/basicauth/BasicAuthenticationConfigurer.java
+++ server/src/main/java/run/ikaros/server/security/authentication/basicauth/BasicAuthenticationConfigurer.java
@@ -1,14 +1,22 @@
 package run.ikaros.server.security.authentication.basicauth;
 
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.web.server.SecurityWebFiltersOrder;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.stereotype.Component;
 import run.ikaros.server.security.authentication.SecurityConfigurer;
 
 @Component
 public class BasicAuthenticationConfigurer implements SecurityConfigurer {
+    private final BasicAuthenticationFilter basicAuthenticationFilter;
+
+    public BasicAuthenticationConfigurer(BasicAuthenticationFilter basicAuthenticationFilter) {
+        this.basicAuthenticationFilter = basicAuthenticationFilter;
+    }
+
     @Override
     public void configure(ServerHttpSecurity http) {
-        http.httpBasic(Customizer.withDefaults());
+        http.httpBasic(Customizer.withDefaults())
+            .addFilterAfter(basicAuthenticationFilter, SecurityWebFiltersOrder.AUTHENTICATION);
     }
 }
