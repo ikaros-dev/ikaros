b10fdaa1640943067468a8f450ff242876f32596
ChiveHao <chivehao@ikaros.run>
2024-10-19 01:43:51 +0800

    feat: mail notify and app link to (#706)
    * feat: 添加剧集更新邮件通知
    
    * feat: 添加app跳转页面
    
    * docs: update CHANGELOG.MD

--- server/src/main/java/run/ikaros/server/core/notify/NotifyService.java
+++ server/src/main/java/run/ikaros/server/core/notify/NotifyService.java
@@ -1,9 +1,12 @@
 package run.ikaros.server.core.notify;
 
+import org.thymeleaf.context.Context;
 import reactor.core.publisher.Mono;
 
 public interface NotifyService {
     Mono<Void> sendMail(String title, String context);
+
+    Mono<Void> send(String title, String template, Context context);
 
     Mono<Void> testMail();
 }
