6a938a966accd189f9b2539fabd052a2085aad2c
ChiveHao <chivehao@ikaros.run>
2025-01-09 01:32:45 +0800

    fix: create subject tag exception when user id not found (#761)
    * fix: create subject tag exception when user id not found in security context.
    
    * docs: update CHANGELOG.MD

--- server/src/main/java/run/ikaros/server/core/tag/TagEndpoint.java
+++ server/src/main/java/run/ikaros/server/core/tag/TagEndpoint.java
@@ -8,6 +8,7 @@ import io.swagger.v3.oas.annotations.enums.ParameterIn;
 import java.util.Optional;
 import lombok.extern.slf4j.Slf4j;
 import org.springdoc.webflux.core.fn.SpringdocRouteBuilder;
+import org.springframework.security.authentication.AuthenticationCredentialsNotFoundException;
 import org.springframework.stereotype.Component;
 import org.springframework.util.Assert;
 import org.springframework.web.reactive.function.server.RouterFunction;
@@ -151,7 +152,9 @@ public class TagEndpoint implements CoreEndpoint {
     private Mono<ServerResponse> create(ServerRequest request) {
         return request.bodyToMono(Tag.class)
             .flatMap(tag -> userService.getUserIdFromSecurityContext()
-                .map(tag::setUserId))
+                .map(tag::setUserId)
+                .onErrorResume(AuthenticationCredentialsNotFoundException.class,
+                    e -> Mono.just(tag)))
             .flatMap(tagService::create)
             .flatMap(tag -> ServerResponse.ok().bodyValue(tag));
     }
