7ff6f6a6d8cef63aed1c040938fb36e6181bc71e
chivehao <chivehao@ikaros.run>
2025-03-29 07:03:54 +0800

    feat: add collection search endpoint in CollectionEndpoint

--- server/src/main/java/run/ikaros/server/core/collection/CollectionEndpoint.java
+++ server/src/main/java/run/ikaros/server/core/collection/CollectionEndpoint.java
@@ -12,7 +12,9 @@ import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
 import reactor.core.publisher.Mono;
 import run.ikaros.api.constant.OpenApiConst;
+import run.ikaros.api.store.enums.CollectionCategory;
 import run.ikaros.api.store.enums.CollectionType;
+import run.ikaros.api.wrap.PagingWrap;
 import run.ikaros.server.endpoint.CoreEndpoint;
 
 @Slf4j
@@ -40,6 +42,44 @@ public class CollectionEndpoint implements CoreEndpoint {
                         .implementation(CollectionType.class))
             )
 
+            .GET("/collections/condition", this::getCollectionsWithCondition,
+                builder -> builder.operationId("GetCollectionsWithCondition")
+                    .tag(tag).description("Get collections with conditions.")
+                    .parameter(parameterBuilder()
+                        .name("category").description("Collection category")
+                        .implementation(CollectionCategory.class))
+                    .parameter(parameterBuilder()
+                        .name("type")
+                        .description("Collection type, default is not done.")
+                        .implementation(CollectionType.class))
+                    .parameter(parameterBuilder()
+                        .name("page")
+                        .description("第几页，从1开始, 默认为1.")
+                        .implementation(Integer.class))
+                    .parameter(parameterBuilder()
+                        .name("size")
+                        .description("每页条数，默认为10.")
+                        .implementation(Integer.class))
+                    .parameter(parameterBuilder()
+                        .name("keyword")
+                        .description("经过Basic64编码的关键词，"
+                            + "不同类型模糊查询不同字段，一般是条目名称或者剧集名称字段。")
+                        .implementation(String.class))
+                    .parameter(parameterBuilder()
+                        .name("nsfw")
+                        .description("Not Safe/Suitable For Work. default is false.")
+                        .implementation(Boolean.class))
+                    .parameter(parameterBuilder()
+                        .name("time")
+                        .implementation(String.class)
+                        .description("时间范围，格式范围类型: 2000.9-2010.8 或者 单个类型2020.8"))
+                    .parameter(parameterBuilder()
+                        .name("updateTimeDesc")
+                        .implementation(Boolean.class)
+                        .description("是否根据更新时间倒序，默认为 true."))
+                    .response(responseBuilder().implementation(PagingWrap.class))
+            )
+
             .build();
     }
 
@@ -48,5 +88,10 @@ public class CollectionEndpoint implements CoreEndpoint {
         Long subjectId = Long.parseLong(id);
         return collectionService.findTypeBySubjectId(subjectId)
             .flatMap(collectionType -> ServerResponse.ok().bodyValue(collectionType));
+    }
+
+    private Mono<ServerResponse> getCollectionsWithCondition(ServerRequest serverRequest) {
+        // todo impl collection search endpoint
+        return Mono.empty();
     }
 }
