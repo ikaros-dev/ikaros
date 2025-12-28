545280fe12cda05c133d4cfdf1eae1209f56f8fc
chivehao <chivehao@ikaros.run>
2025-12-28 23:07:38 +0800

    feat: 视频字幕接口的字幕URL更新为动态解析的流URL

--- api/src/main/java/run/ikaros/api/constant/OpenApiConst.java
+++ api/src/main/java/run/ikaros/api/constant/OpenApiConst.java
@@ -4,4 +4,6 @@ public interface OpenApiConst {
     String CORE_GROUP = "core.ikaros.run";
     String PLUGIN_GROUP = "plugin.ikaros.run";
     String CORE_VERSION = "v1alpha1";
+    String ATT_STREAM_ENDPOINT_PREFIX =
+        "/api/" + OpenApiConst.CORE_VERSION + "/attachment/stream/id";
 }
