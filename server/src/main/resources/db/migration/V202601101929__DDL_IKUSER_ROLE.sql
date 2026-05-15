c89d13e65d28bd8f5866027d079734e157e6bc67
chivehao <chivehao@ikaros.run>
2026-05-15 22:01:12 +0800

    feat: ddl

--- server/src/main/resources/db/migration/V202601101929__DDL_IKUSER_ROLE.sql
+++ server/src/main/resources/db/migration/V202601101929__DDL_IKUSER_ROLE.sql
@@ -1,8 +1,8 @@
 create table if not exists "ikuser_role"
 (
     "id"      uuid primary key DEFAULT uuidv7(),
-    "user_id" uuid,
-    "role_id" uuid,
+    "user_id" uuid not null,
+    "role_id" uuid not null,
     CONSTRAINT "user_role_id_uk" unique (user_id, role_id)
 )
 ;
