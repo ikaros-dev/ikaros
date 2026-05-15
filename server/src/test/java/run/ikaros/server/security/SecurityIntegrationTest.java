c74063829e69c6725d91029510153ad9c70e18fd
chivehao <chivehao@ikaros.run>
2026-05-15 10:38:52 +0800

    test: add cases

--- server/src/test/java/run/ikaros/server/security/SecurityIntegrationTest.java
+++ server/src/test/java/run/ikaros/server/security/SecurityIntegrationTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
@@ -26,6 +27,7 @@ import org.testcontainers.junit.jupiter.Testcontainers;
     }
 )
 @AutoConfigureMockMvc
+@Import(SecurityIntegrationTest.StubController.class)
 class SecurityIntegrationTest {
 
     @Container
