415cff51b6d0620e15086ebf793fdb0c011646f5
chivehao <chivehao@ikaros.run>
2026-05-17 23:23:14 +0800

    feat: add new R2dbcEntityTemplate bean for migration.

---
+++ server/src/main/java/run/ikaros/server/migration/MigrationConfiguration.java
@@ -0,0 +1,32 @@
+package run.ikaros.server.migration;
+
+import io.r2dbc.spi.ConnectionFactory;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.r2dbc.core.R2dbcEntityTemplate;
+
+@Configuration(proxyBeanMethods = false)
+@EnableConfigurationProperties(MigrationProperties.class)
+public class MigrationConfiguration {
+
+    /**
+     * Create migration connection factory from ikaros.migration.datasource properties.
+     */
+    @Bean("migrationConnectionFactory")
+    @ConditionalOnProperty(prefix = "ikaros.migration.datasource", name = "url")
+    public ConnectionFactory migrationConnectionFactory(MigrationProperties properties) {
+        return MigrationConnectionFactoryBuilder.build(properties);
+    }
+
+    /**
+     * Create R2dbcEntityTemplate backed by the migration connection factory.
+     */
+    @Bean("migrationR2dbcEntityTemplate")
+    @ConditionalOnProperty(prefix = "ikaros.migration.datasource", name = "url")
+    public R2dbcEntityTemplate migrationR2dbcEntityTemplate(
+        ConnectionFactory migrationConnectionFactory) {
+        return new R2dbcEntityTemplate(migrationConnectionFactory);
+    }
+}
