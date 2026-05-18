57118360a27662b2e84c8e6a175622fefe757192
chivehao <chivehao@ikaros.run>
2026-05-18 12:13:27 +0800

    fix: 当redis被禁用时，排除redis健康检测的自动配置类 (#835)

--- server/src/main/java/run/ikaros/server/cache/RedisAutoConfigDisableConfiguration.java
+++ server/src/main/java/run/ikaros/server/cache/RedisAutoConfigDisableConfiguration.java
@@ -2,12 +2,14 @@ package run.ikaros.server.cache;
 
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.data.redis.autoconfigure.DataRedisReactiveAutoConfiguration;
+import org.springframework.boot.data.redis.autoconfigure.health.DataRedisReactiveHealthContributorAutoConfiguration;
 import org.springframework.context.annotation.Conditional;
 import org.springframework.context.annotation.Configuration;
 import run.ikaros.server.cache.condition.CacheRedisDisableCondition;
 
 @Configuration(proxyBeanMethods = false)
 @Conditional(CacheRedisDisableCondition.class)
-@EnableAutoConfiguration(exclude = {DataRedisReactiveAutoConfiguration.class})
+@EnableAutoConfiguration(exclude = {DataRedisReactiveAutoConfiguration.class,
+    DataRedisReactiveHealthContributorAutoConfiguration.class})
 public class RedisAutoConfigDisableConfiguration {
 }
