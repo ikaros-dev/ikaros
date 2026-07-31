f1b341ae2e975776d7d5b212e4e4d0f0cfc67902
wjz2001 <wjz2001@users.noreply.github.com>
2026-07-31 11:55:25 +0800

    fix(console): 补充附件令牌填写提示

--- console/src/modules/content/attachment/AttachmentDriverPost.vue
+++ console/src/modules/content/attachment/AttachmentDriverPost.vue
@@ -36,6 +36,8 @@ const driver = ref<AttachmentDriver>({
 
 const driverBtnLoading = ref(false);
 const driverElFormRef = ref<FormInstance>();
+const accessTokenHintVisible = ref(false);
+const refreshTokenHintVisible = ref(false);
 const driverFormRules = reactive<FormRules>({
 	type: [
 		{
@@ -197,7 +199,11 @@ onMounted(async () => {
 						v-model="driver.access_token"
 						clearable
 						placeholder="n28uf.47fe03dd47f06d07fec0e88860cc2e9e.8b2b8261cef0a0b7e38e3c96277d62e3aba29908695df66f4383fce100731ce3"
+						@focus="accessTokenHintVisible = true"
 					></el-input>
+					<div v-if="accessTokenHintVisible" class="form-item-hint">
+						{{ t('module.attachment.driver.post.message.hint.token_optional') }}
+					</div>
 				</el-form-item>
 				<el-form-item
 					:label="t('module.attachment.driver.table.colum.label.refresh_token')"
@@ -206,7 +212,11 @@ onMounted(async () => {
 						v-model="driver.refresh_token"
 						clearable
 						placeholder="n28uf.c9a4974d851f9aa42b4edd523847dadf06d84914581d05a093e532c1eeec0ced.aec3da5a8f6c72c3623f430f23b885d906eef639b18fd1be42f9810ac1880ce2"
+						@focus="refreshTokenHintVisible = true"
 					></el-input>
+					<div v-if="refreshTokenHintVisible" class="form-item-hint">
+						{{ t('module.attachment.driver.post.message.hint.token_optional') }}
+					</div>
 				</el-form-item>
 				<el-form-item
 					:label="t('module.attachment.driver.table.colum.label.comment')"
@@ -227,4 +237,14 @@ onMounted(async () => {
 	</el-row>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.form-item-hint {
+	position: absolute;
+	top: 100%;
+	left: 0;
+	padding-top: 2px;
+	font-size: 12px;
+	line-height: 1;
+	color: var(--el-color-success);
+}
+</style>
