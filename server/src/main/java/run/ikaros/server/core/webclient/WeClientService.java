4db0a6d4bbccf3ff58a46f02034d7642157948b2
wjz2001 <wjz2001@users.noreply.github.com>
2026-08-13 20:50:04 +0800

    refactor(web): 为 HTTP 客户端包启用默认非空

--- server/src/main/java/run/ikaros/server/core/webclient/WeClientService.java
+++ server/src/main/java/run/ikaros/server/core/webclient/WeClientService.java
@@ -1,7 +1,6 @@
 package run.ikaros.server.core.webclient;
 
 import jakarta.validation.constraints.NotBlank;
-import org.jspecify.annotations.NonNull;
 import org.springframework.resilience.annotation.Retryable;
 import reactor.core.publisher.Mono;
 import run.ikaros.api.core.attachment.Attachment;
@@ -13,7 +12,6 @@ public interface WeClientService {
      * @param url network image url
      * @return new file url in file system.
      */
-    @NonNull
     @Retryable
     Mono<Attachment> downloadImageWithGet(@NotBlank String policy,
                                           @NotBlank String url);
---
+++ server/src/main/java/run/ikaros/server/core/webclient/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * 提供服务端 HTTP 客户端配置与访问能力.
+ */
+@NullMarked
+package run.ikaros.server.core.webclient;
+
+import org.jspecify.annotations.NullMarked;
