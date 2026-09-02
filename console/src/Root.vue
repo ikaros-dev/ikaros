cc11bd6c965c5ccde91f563d3fee426346705623
chivehao <chivehao@ikaros.run>
2026-09-03 00:21:51 +0800

    feat(console): add shared material navigation drawer

--- console/src/Root.vue
+++ console/src/Root.vue
@@ -1,1 +1,9 @@
-<template><router-view /></template>
+<script setup lang="ts">
+import { computed } from 'vue'
+import { useRoute } from 'vue-router'
+import ConsoleNav from './ConsoleNav.vue'
+
+const route = useRoute()
+const standalone = computed(() => route.path.startsWith('/console/') && !['/console/dashboard', '/console/search', '/console/activity', '/console/security/users', '/console/security/permissions', '/console/security/sessions'].includes(route.path))
+</script>
+<template><div :class="{ 'with-console-nav': standalone }"><ConsoleNav v-if="standalone" /><router-view /></div></template>
