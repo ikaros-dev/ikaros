dac6b0578befb768ca69344025887a8ce16b7d81
chivehao <chivehao@ikaros.run>
2026-09-02 21:10:23 +0800

    feat(v2): add search failure reconciliation

--- src/main/java/run/ikaros/search/SearchProjectionFailureRepository.java
+++ src/main/java/run/ikaros/search/SearchProjectionFailureRepository.java
@@ -2,6 +2,11 @@ package run.ikaros.search;
 
 import java.util.UUID;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 public interface SearchProjectionFailureRepository
-    extends ReactiveCrudRepository<SearchProjectionFailureEntity, UUID> { }
+    extends ReactiveCrudRepository<SearchProjectionFailureEntity, UUID> {
+    Flux<SearchProjectionFailureEntity> findByResolvedAtIsNullOrderByFailedAtAsc();
+    Mono<SearchProjectionFailureEntity> findByIdAndResolvedAtIsNull(UUID id);
+}
