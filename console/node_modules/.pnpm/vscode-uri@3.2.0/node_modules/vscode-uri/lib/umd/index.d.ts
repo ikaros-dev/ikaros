9d7dbe61694024214cc373f3a80f3f436c44da68
chivehao <chivehao@ikaros.run>
2026-09-02 22:53:03 +0800

    chore(console): ignore generated frontend artifacts

--- console/node_modules/.pnpm/vscode-uri@3.2.0/node_modules/vscode-uri/lib/umd/index.d.ts
+++
@@ -1,8 +0,0 @@
-import { URI } from './uri';
-import { Utils } from './utils';
-export { URI, Utils };
-declare const _default: {
-    URI: typeof URI;
-    Utils: typeof Utils;
-};
-export default _default;
