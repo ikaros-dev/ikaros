71438181703a439ff528dbb1043f019782024969
chivehao <chivehao@ikaros.run>
2026-09-03 20:19:54 +0800

    fix(console): disable fake backend routes

--- console/build/plugins.ts
+++ console/build/plugins.ts
@@ -13,7 +13,6 @@ import { visualizer } from "rollup-plugin-visualizer";
 import removeConsole from "vite-plugin-remove-console";
 import VueI18nPlugin from "@intlify/unplugin-vue-i18n/vite";
 import { codeInspectorPlugin } from "code-inspector-plugin";
-import { vitePluginFakeServer } from "vite-plugin-fake-server";
 
 export function getPluginsList(
   VITE_CDN: boolean,
@@ -45,13 +44,6 @@ export function getPluginsList(
      * vite-plugin-router-warn只在开发环境下启用，只处理vue-router文件并且只在服务启动或重启时运行一次，性能消耗可忽略不计
      */
     removeNoMatch(),
-    // mock支持
-    vitePluginFakeServer({
-      logger: false,
-      include: "mock",
-      infixName: false,
-      enableProd: true
-    }),
     // svg组件化支持
     svgLoader(),
     // 自动按需加载图标
