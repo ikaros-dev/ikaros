3a18bbad2ce260fd49b64141982f95f96411fba7
chivehao <chivehao@ikaros.run>
2026-09-04 21:46:10 +0800

    style: 更新图标

--- console/src/layout/hooks/useNav.ts
+++ console/src/layout/hooks/useNav.ts
@@ -140,7 +140,7 @@ export function useNav() {
 
   /** 获取`logo` */
   function getLogo() {
-    return new URL("/logo.svg", import.meta.url).href;
+    return new URL("/logo.png", import.meta.url).href;
   }
 
   return {
